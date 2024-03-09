14bd9f017ed8f8ddce3688f7fe6e69f7782806bb
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-03-09 22:56:39 +0545

    updated code

--- camera.py
+++ camera.py
@@ -32,77 +32,6 @@ global cap1
 show_text = [0]
 
 
-''' Class for calculating FPS while streaming. Used this to check performance of using another thread for video streaming '''
-
-
-class FPS:
-    def __init__(self):
-        # store the start time, end time, and total number of frames
-        # that were examined between the start and end intervals
-        self._start = None
-        self._end = None
-        self._numFrames = 0
-
-    def start(self):
-        # start the timer
-        self._start = datetime.datetime.now()
-        return self
-
-    def stop(self):
-        # stop the timer
-        self._end = datetime.datetime.now()
-
-    def update(self):
-        # increment the total number of frames examined during the
-        # start and end intervals
-        self._numFrames += 1
-
-    def elapsed(self):
-        # return the total number of seconds between the start and
-        # end interval
-        return (self._end - self._start).total_seconds()
-
-    def fps(self):
-        # compute the (approximate) frames per second
-        return self._numFrames / self.elapsed()
-
-
-''' Class for using another thread for video streaming to boost performance '''
-
-
-class WebcamVideoStream:
-
-    def __init__(self, src=0):
-        self.stream = cv2.VideoCapture(src, cv2.CAP_DSHOW)
-        (self.grabbed, self.frame) = self.stream.read()
-        self.stopped = False
-
-    def start(self):
-        # start the thread to read frames from the video stream
-        Thread(target=self.update, args=()).start()
-        return self
-
-    def update(self):
-        # keep looping infinitely until the thread is stopped
-        while True:
-            # if the thread indicator variable is set, stop the thread
-            if self.stopped:
-                return
-            # otherwise, read the next frame from the stream
-            (self.grabbed, self.frame) = self.stream.read()
-
-    def read(self):
-        # return the frame most recently read
-        return self.frame
-
-    def stop(self):
-        # indicate that the thread should be stopped
-        self.stopped = True
-
-
-''' Class for reading video stream, generating prediction and recommendations '''
-
-
 class VideoCamera(object):
 
     def get_frame(self):
@@ -111,15 +40,11 @@ class VideoCamera(object):
         json_file = open('emotion_model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
-        cap1 = WebcamVideoStream(src=0)
-        frame = cap1.read()
+        cap1 = cv2.VideoCapture(1)
+        success, frame = cap1.read()
         image = cv2.resize(frame, (600, 500))
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
-        df1 = pd.read_csv(music_dict[show_text[0]])
-        df1 = df1[['uri']]
-        random_number = np.random.randint(10, 20)
-        df1 = df1.head(random_number)
         emotion_model = model_from_json(loaded_model_json)
         emotion_model.load_weights("emotion_model.h5")
         for (x, y, w, h) in face_rects:
